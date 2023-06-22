3f2d5604c3e40e186197c34b399b7da44c210959
Mahdi <mahdijm.bb@gmail.com>
2023-06-22 17:51:30 +0330

    initial state of clients

---
+++ clients/Client4.java
@@ -0,0 +1,23 @@
+package clients;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client4 {
+    public static void main(String[] args) throws IOException {
+        Socket clientSocket = new Socket("127.0.0.1",6666);
+        System.out.println("client connected !!! ");
+        PrintWriter printWriter= new PrintWriter(clientSocket.getOutputStream(),true);
+        Scanner in = new Scanner(System.in);
+        String test = "";
+        do{
+            test = in.nextLine();
+            printWriter.println(test);
+        }while (!test.equals("END"));
+        System.out.println("Disconnected!!");
+        printWriter.close();
+        clientSocket.close();
+    }
+}
